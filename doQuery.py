1f1a1b6f8e0321b1e4dd7041b398915696e5279e
blm <mcleanb@photo-nomad.net>
2023-06-19 19:22:02 -0400

    more tests and models

--- doQuery.py
+++ doQuery.py
@@ -10,12 +10,29 @@ openai.api_key = "not needed for a local LLM"
 # Set up the prompt and other parameters for the API request
 prompts = [
     "sisters_age: when I was 6, my sister was half my age. Now I am 80. How old is my sister now? Calculate this step by step.",
-    "js_ascii: write a JavaScript program to generate and print out the ascii character and hexadecimal codes for 0 to 7f"
+    "js_ascii: write a JavaScript program to generate and print out the ascii character and hexadecimal codes for 0 to 7f",
+    "dry_socks: Tom washed 10 pairs of socks. The socks are now wet from the wash, so Tom hangs the 10 pairs of socks outside to dry. 10 hours later, he comes back outside to check on the socks. He feels each sock and notices they are all dry. He takes them back inside because they are all dry. How long will it take Tom to dry seven pairs of socks presuming the same weather conditions?" ,
+    "TOT_socks: Three experts with exceptional logical thinking skills are collaboratively answering a question using a Tree of Thoughts method. Each expert will share their thought process in detail, taking into account the previous thoughts of others and emitting any errors. They will iteratively refine and expand upon each other's ideas, giving credit where it's due. The process continues until a conclusive answer is found. Organize the entire response in a markdown format. The question is, \"Tom washed 10 pairs of socks. The socks are now wet from the wash, so Tom hangs the 10 pairs of socks outside to dry. 10 hours later, he comes back outside to check on the socks. He feels each sock and notices they are all dry. He takes them back inside because they are all dry. How long will it take Tom to dry seven pairs of socks presuming the same weather conditions?\"",
+    "railway: Explain origin of standard gauge for railways",
+    "math_X_power: In 8^X+2^X=130 show how to solve for X",
+    "math_qubic: in ax^3+bx^2+c*x+d=0, use the cardano's formula to get the value for x where a=1, b=0, c=1 and d=-130",
+    "math_qubic_simple: in x^3+x-130=0, use the cardano's formula to get the value for x"
+    "python_math: in ax^3+bx^2+c*x+d=0, write a python program to solve for all possible values of x",
 ]
 
 # model = "gpt-3.5-turbo"
 # model = "mpt-7b-chat"
-models = ["gpt4all-j-v1.3-groovy", "mpt-7b-chat"]
+models = [
+    "gpt4all-j-v1.3-groovy",
+    "mpt-7b-chat",
+    "ggml-wizardLM-7B.ggmlv3.q8_0",
+    "ggml-vic7b-q5_1",
+    "ggml-vic13b-q8_0",
+    "ggml-nous-gpt4-vicuna-13b",
+    "ggml-nous-hermes-13b.ggmlv3.q6_K"
+    "ggml-30b-Lazarus.ggmlv3.q5_1",
+    "ggml-airoboros-33b-gpt4-1.2.ggmlv3.q4_1",
+]
 
 
 def queryModel(model, prompt):

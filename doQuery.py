23d531ca595b8d086abb5ccca7bb6c255699387c
blm <mcleanb@photo-nomad.net>
2023-06-19 14:09:45 -0400

    added iteration and saving results to file

--- doQuery.py
+++ doQuery.py
@@ -1,4 +1,5 @@
 import openai
+import os
 
 openai.api_base = "http://localhost:4891/v1"
 # openai.api_base = "https://api.openai.com/v1"
@@ -6,8 +7,8 @@ openai.api_base = "http://localhost:4891/v1"
 openai.api_key = "not needed for a local LLM"
 
 # Set up the prompt and other parameters for the API request
-prompts = ["write a JavaScript program to generate and print out the ascii character and hexadecimal codes for 0 to 7f",
-           "when I was 6, my sister was half my age. Now I am 80. How old is my sister now? Calculate this step by step."]
+prompts = ["js_ascii: write a JavaScript program to generate and print out the ascii character and hexadecimal codes for 0 to 7f",
+           "sisters_age: when I was 6, my sister was half my age. Now I am 80. How old is my sister now? Calculate this step by step."]
 
 # model = "gpt-3.5-turbo"
 # model = "mpt-7b-chat"
@@ -19,7 +20,7 @@ def queryModel(model, prompt):
     response_ = openai.Completion.create(
         model=model,
         prompt=prompt,
-        max_tokens=50,
+        max_tokens=4096,
         temperature=0.28,
         top_p=0.95,
         n=1,
@@ -30,11 +31,29 @@ def queryModel(model, prompt):
 
 
 for model in models:
-    for prompt in prompts:
-        print("Testing model", model, " and prompt", prompt)
-        response = queryModel(model, prompt)
+    # Create a new directory
+    modelPath = "data/" + model
+    if not os.path.exists(modelPath):
+        os.mkdir(modelPath)
 
-        # Print the generated completion
-        print("Response", response)
+    for i in range(len(prompts)):
+        parts = prompts[i].split(": ")
+        fileName, prompt = parts
+        filePath = modelPath + "/" + fileName + ".txt"
+
+        if os.path.exists(filePath):
+            print("Already have results for", filePath, "skipping")
+        else:
+            print("Testing", i, "model", model, ", and prompt: ", prompt)
+            response = queryModel(model, prompt)
+    
+            # Print the generated completion
+            print("Response", response)
+    
+            # Open a file for writing using with statement
+            print("Saving to", filePath)
+            with open(filePath, "w") as file:
+                # Write some text to the file
+                file.write(response)
 
 print("Done")

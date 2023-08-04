be7dcebaca64f1152674a5780f07d1d2df9a0eca
blm <bruce.mclean@unfoldingWord.org>
2023-08-04 14:47:52 -0400

    coding

--- test_math.py
+++ test_math.py
@@ -1,15 +1,29 @@
-def largest_difference(numbers):
-    smallest = float('inf')
-    largest = float('-inf')
-    for num in numbers:
-        if num < smallest:
-            smallest = num
-        elif num > largest:
-            largest = num
-    return abs(largest - smallest)
 
-numbers = [3, 5, 7, 10, 12]
-print(largest_difference(numbers)) # Output: 4
+def sieve_of_eratosthenes(n):
+    # Create a boolean array "prime[0..n]" and initialize all entries it as true. A value in prime[i] will finally be false if i is Not a prime, else true.
+    primes = [True for _ in range(n+1)]
+
+    p = 2
+
+    while (p * p <= n):
+        # If prime[p] is not changed, then it is a prime
+        if (primes[p] == True):
+            # Update all multiples of p
+            for i in range(p*2, n+1, p):
+                primes[i] = False
+
+        p += 1
+
+    return [x for x in range(n+1) if primes[x]]
+
+primes_list = sieve_of_eratosthenes(100*10)
+print("The first hundred prime numbers are:", end=" ")
+for i, num in enumerate(primes_list):
+    print(num, end=", "*(i!=99)) # Print comma separated values except for the last one.
+
+print("")
+print("count=", len(primes_list))
+# Output: 4
     
     
 # def read_spreadsheet(filepath):

f4a00983b66d7d852ba30ceb44aac80e6c225190
blm <bruce.mclean@unfoldingword.org>
2023-09-29 07:56:14 -0700

    added test

--- doQuery.py
+++ doQuery.py
@@ -437,7 +437,7 @@ models = [
     # "codellama-13b-instruct.Q4_K_M.gguf",
     # "llama-2-13b-chat.ggmlv3.q4_0",
     # "codellama-13b-python.Q4_K_M.gguf",
-    "mistral-7b-instruct-v0.1.Q6_K.gguf"
+    # "mistral-7b-instruct-v0.1.Q6_K.gguf"
 ]
 
 ALPACA_PROMPT = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n%prompt%\n\n### Response:\n"
--- test.js
+++ test.js
@@ -1,38 +1,16 @@
-function extractCode(filename) {
-    const regex = /^[0-9]+-(.)\w{2}\.(usfm)$/; // Matches a three character code followed by .usfm extension
-    const match = filename.match(regex);
-    if (match && match[1]) {
-        return match[1];
-    } else {
-        throw new Error('Invalid file name format');
-    }
-}
+let dict = {
+    "zebra": "animal",
+    "apple": "fruit",
+    "banana": "fruit",
+    "carrot": "vegetable"
+};
 
+let sortedDict = {};
+Object.keys(dict).sort().forEach((key) => {
+    sortedDict[key] = dict[key];
+});
 
-console.log(extractCode('57-1Jn.usfm')); // Outputs "1Jn"
-console.log(extractCode('01-Exo.usfm')); // Outputs "Exo"
-console.log(extractCode('23-Psa.USFM')); // Outputs "Psa"
-
-//
-// const first100Primes = generatePrimes(1542); // 154 is the smallest number that's greater than or equal to 100 and an even number
-// console.log(first100Primes);
-
-function isPrime(num) {
-    for(let i = 2; i < num; i++)
-        if(num % i === 0) return false;
-    return num !== 1;
-}
-
-let count = 0;
-let number = 2;
-while (count < 100) {
-    if (isPrime(number)) {
-        console.log(number);
-        count++;
-    }
-    number++;
-}
-
+console.log(sortedDict);
 //
 // This code will create a dictionary with three keys and values, and then use the `sort()` method to sort it based on the difference between each key and its corresponding value. The sorted dictionary will be stored in the `myDict` variable.
 //
